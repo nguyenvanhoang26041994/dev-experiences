37330ed9e5fa74a8d64bca9311e4ce5e996d5078
Van Hoang <hoang.nguyen3@tiki.vn>
2018-08-25 10:53:41 +0700

    OK

--- app/containers/App/selectors.js
+++
@@ -1,8 +0,0 @@
-import { createSelector } from 'reselect';
-
-const selectRoute = state => state.route;
-
-const makeSelectLocation = () =>
-  createSelector(selectRoute, routeState => routeState.location);
-
-export { makeSelectLocation };
--- app/containers/App/tests/selectors.test.js
+++
@@ -1,9 +0,0 @@
-import { makeSelectLocation } from 'containers/App/selectors';
-
-describe('makeSelectLocation', () => {
-  it('should select the location', () => {
-    const route = { location: { pathname: '/foo' } };
-    const mockedState = { route };
-    expect(makeSelectLocation()(mockedState)).toEqual(route.location);
-  });
-});
