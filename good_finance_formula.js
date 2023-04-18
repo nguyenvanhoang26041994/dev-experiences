724480ba04105b0b459694a100118aa48c25e949
Nguyễn Văn Hoàng <nguyenvanhoang26041994@gmail.com>
2023-04-19 01:08:22 +0700

    Update good_finance_formula.js

--- good_finance_formula.js
+++ good_finance_formula.js
@@ -96,7 +96,7 @@ function good_xrp_finance_formular({
 
 good_xrp_finance_formular({
   your_birth_date: new Date('06/05/1994'), // mm/DD/yyyy
-  current_date: new Date('06/05/2025'),
+  current_date: new Date('06/05/2024'),
   your_age_that_you_suppose_to_run_out_of_xrp: 60,
   your_current_xrp_amount: 50650,
   current_xrp_usd_price: 0.52,
